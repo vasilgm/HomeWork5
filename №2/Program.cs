d9f38a0cd6f5f48e088deff268f6ea1739ac0117
Vasil <vasilgm@yandex.ru>
2023-04-09 14:00:20 +0300

    second №2

--- №2/Program.cs
+++ №2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] array = new int[number("длинну массива")];
+RandomArray(array);
+System.Console.WriteLine(" ");
+System.Console.WriteLine($"Сумма элементов стоящих на нечетных позициях равна {sum (array)}");
+
+
+int number (string i)
+{
+    System.Console.Write($"Введите {i} : ");
+    int x = int.Parse(Console.ReadLine());
+    return x;
+}
+
+int[] RandomArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+        System.Console.Write($"{array[i]} ");
+    }
+    return array;
+}
+
+int sum (int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum = array[i] + sum;
+    }
+    return sum;
+}
--- №3/Program.cs
+++ №3/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
